f4ec8f7a55d343fab65983735eacd95e0068fd47
Nicholas1811 <nicholassdl660@gmail.com>
2020-11-16 12:03:38 +0800

    commit

--- obkd.py
+++ obkd.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for,request
 import requests
-
+#py script. add in your codes here.
 app = Flask(__name__)
 @app.route("/index", methods = ['GET'])
 def index():
