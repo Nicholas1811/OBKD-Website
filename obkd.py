13786e5993c6d87c3f75b99da4d689203ec434ac
Nicholas1811 <nicholassdl660@gmail.com>
2020-11-16 19:40:32 +0800

    ims

--- obkd.py
+++ obkd.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for,request
 import requests
-#py script. add in your codes here.
+#py script. add in your codes here. default page is index so when you run on 127.0.0.1:5000, do include the route at the back.
+#For example, on browser, 127.0.0.1:5000/index
 app = Flask(__name__)
 @app.route("/index", methods = ['GET'])
 def index():
