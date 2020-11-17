d11ddeb57f674ca78a0f890022e7936b745feff8
Nicholas1811 <nicholassdl660@gmail.com>
2020-11-17 16:53:30 +0800

    push for nav bar

--- obkd.py
+++ obkd.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, redirect, url_for,request
 import requests
 #py script. add in your codes here. default page is index so when you run on 127.0.0.1:5000, do include the route at the back.
-#For example, on browser, 127.0.0.1:5000/index
+#For example, on browser, 127.0.0.1:5000/
 app = Flask(__name__)
 @app.route("/index", methods = ['GET'])
 def index():
     return render_template("index.html")
+@app.route("/aboutUs", methods = ['GET','POST'])
+def aboutUs():
+    return render_template("about.html")
